0822e6b2225ea5226172adfa152ecaf9d1d954c5
alisam163 <97351388+alisam163@users.noreply.github.com>
2022-08-17 10:56:12 +0400

    form registration css complete

--- obnp/home/forms.py
+++ obnp/home/forms.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class RegistrationUserForm(forms.Form):
     username = forms.CharField(max_length=30, label='Логин')
-    email = forms.CharField(max_length=100, label='Электронная почта')
+    email = forms.CharField(max_length=100, label='Электронная почта ')
     password1 = forms.CharField(min_length=6, widget=forms.PasswordInput(), label='Пароль')
     password2 = forms.CharField(min_length=6, widget=forms.PasswordInput(), label='Повтор пароля')
 
