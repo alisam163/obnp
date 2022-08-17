47be3a1d4c05e3dc4f05837509c6f6c6f20895d4
alisam163 <97351388+alisam163@users.noreply.github.com>
2022-08-17 15:00:39 +0400

    закончена форма регистрации. css and backend

--- obnp/home/forms.py
+++ obnp/home/forms.py
@@ -1,11 +1,24 @@
+from email.policy import default
+from xml.dom import ValidationErr
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 
 class RegistrationUserForm(forms.Form):
+    TERMS_OF_USE = (
+        ('yes', ''),
+    )
+
     username = forms.CharField(max_length=30, label='Логин')
     email = forms.CharField(max_length=100, label='Электронная почта ')
     password1 = forms.CharField(min_length=6, widget=forms.PasswordInput(), label='Пароль')
     password2 = forms.CharField(min_length=6, widget=forms.PasswordInput(), label='Повтор пароля')
+    terms = forms.BooleanField()
 
+    def clean(self):
+        password1 = self.cleaned_data['password1']
+        password2 = self.cleaned_data['password2']
+        if password1 != password2:
+            raise ValidationError("Пароли не совпадают. Попробуйте заново")
--- obnp/home/views.py
+++ obnp/home/views.py
@@ -1,8 +1,10 @@
+import imp
 from xml.dom import ValidationErr
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.urls import reverse_lazy
 from .forms import RegistrationUserForm
+from django.contrib.auth.models import User
 # Create your views here.
 
 def sign_in(request):
@@ -19,7 +21,13 @@ def registration(request):
         form = RegistrationUserForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data['username']
-            print(username)
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password1']
+            try:
+                user = User.objects.create_user(username, email, password)
+                user.save
+            except Exception as e:
+                form.add_error(None, 'Что-то пошло не так. Обратитесь к администратору')
     else:
         form = RegistrationUserForm()
     return render(request, 'home/reg.html', {'form':form})
